b285ae37204ca743a5af8fb8898305467399daaa
eunji soh <rekagirl@finalcompany.co.kr>
2017-09-12 17:56:37 +0900

    번역수정

--- src/views/errorPage/404.vue
+++ src/views/errorPage/404.vue
@@ -11,7 +11,7 @@
         <div class="bullshit__oops">OOPS!</div>
         <div class="bullshit__info">版权所有<a class='link-type' href='https://wallstreetcn.com' target='_blank'>华尔街见闻</a></div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
+        <div class="bullshit__info">입력 한 URL이 정확한지 확인하십시오. 다음 버튼을 클릭하여 홈페이지로 돌아가거나 오류 보고서를 보내주십시오.</div>
         <a href="/" class="bullshit__return-home">返回首页</a>
       </div>
     </div>
